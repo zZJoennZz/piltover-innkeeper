6e899e223a40a80938026b3226a012034da6e80c
Joenn Aquilino <zzjoennzz@gmail.com>
2023-07-12 05:11:54 +0800

    New: SASS, preprocess, feathericons

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,20 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
-import { svelte } from '@sveltejs/vite-plugin-svelte';
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
+import { svelte } from "@sveltejs/vite-plugin-svelte";
+import preprocess from "svelte-preprocess";
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: ["resources/css/app.css", "resources/js/app.js"],
             refresh: true,
         }),
-        svelte(),
+        svelte({
+            preprocess: [
+                preprocess({
+                    postcss: true,
+                }),
+            ],
+        }),
     ],
 });
