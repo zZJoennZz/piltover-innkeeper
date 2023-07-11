f548d9f994648807a311b57e22234f384ccb6e22
Joenn Aquilino <zzjoennzz@gmail.com>
2023-07-12 05:59:36 +0800

    New: Client and user middlewares

---
+++ app/Http/Middleware/ClientMiddleware.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class ClientMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        if (auth()->guard('client')->check()) {
+            return $next($request);
+        }
+
+        abort(403, 'Unauthorized');
+    }
+}
