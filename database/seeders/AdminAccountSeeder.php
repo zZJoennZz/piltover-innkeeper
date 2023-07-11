f84a6c8bfbb06ba9c52eefb93c5c76a96235face
Joenn Aquilino <zzjoennzz@gmail.com>
2023-07-12 05:11:26 +0800

    New: Initial admin account

---
+++ database/seeders/AdminAccountSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\User;
+
+class AdminAccountSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+        User::create([
+            'username' => 'admin',
+            'password' => env('ADMIN_INITIAL_PW') ?? 'admin',
+        ]);
+    }
+}
